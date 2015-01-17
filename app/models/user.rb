f75c1acb3dacc5b2310c4d2f3aeaf11e703dbf2d
Sudharsanan <sudhar.ti@gmail.com>
2015-01-17 15:38:58 -0600

    Sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :confirmable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true, length: { maximum: 100 }
+  validates :last_name, presence: true, length: { maximum: 100 }
+
 end
