e204c68819a8d4b6d14ca71ee0a060214c19901c
Sudharsanan <sudhar.ti@gmail.com>
2015-01-17 17:05:02 -0600

    Add authentication as separate model

---
+++ db/migrate/20150117230308_create_authentications.rb
@@ -0,0 +1,13 @@
+class CreateAuthentications < ActiveRecord::Migration
+  def change
+    create_table :authentications do |t|
+      t.string :uid
+      t.string :provider
+      t.string :oauth_token
+      t.references :user, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :authentications, :users
+  end
+end
---
+++ spec/factories/authentications.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :authentication do
+    uid "MyString"
+provider "MyString"
+oauth_token "MyString"
+user nil
+  end
+
+end
