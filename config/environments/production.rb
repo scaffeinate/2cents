44613c78a58c522ff94292c054ac7f7987133d32
Sudharsanan <sudhar.ti@gmail.com>
2015-01-18 12:59:44 -0600

    Production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -76,4 +76,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => "http://two-cents-hack.herokuapp.com/" }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.sendgrid.net",
+    :port                 => "587",
+    :domain               => "heroku.com",
+    :user_name            => ENV["USERNAME"],
+    :password             => ENV["PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
 end
